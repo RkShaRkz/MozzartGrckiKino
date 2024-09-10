22106c197f2b8cc4568395e0dd7af7d4d7cc11b6
RkShaRkz <darksidemetatron@gmail.com>
2024-09-10 22:59:08 +0200

    ActivityMain refactor, starting to integrate FragmentGameList

---
+++ composeApp/src/androidMain/kotlin/org/example/project/ActivityMain.kt
@@ -0,0 +1,53 @@
+package org.example.project
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.List
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainScreen()
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("App Title") },
+                actions = {
+                    IconButton(onClick = { navController.navigate("fragment1") }) {
+                        Icon(Icons.Default.List, contentDescription = "Game List")
+                    }
+                    IconButton(onClick = { navController.navigate("fragment2") }) {
+                        Icon(Icons.Default.Edit, contentDescription = "Fragment 2")
+                    }
+                    IconButton(onClick = { navController.navigate("fragment3") }) {
+                        Icon(Icons.Default.CheckCircle, contentDescription = "Fragment 3")
+                    }
+                }
+            )
+        }
+    ) {
+        NavHost(navController = navController, startDestination = "fragment1") {
+            composable("fragment1") { FragmentGameList(navController) }
+            composable("fragment2") { Fragment2(navController) }
+            composable("fragment3") { Fragment3(navController) }
+        }
+    }
+}
