b98809740da7c82dc6c949228552677a9b7d24d1
RkShaRkz <darksidemetatron@gmail.com>
2024-09-11 19:02:52 +0200

    Add row above the list of games for FragmentGameList

--- composeApp/src/androidMain/kotlin/org/example/project/FragmentGameList.kt
+++ composeApp/src/androidMain/kotlin/org/example/project/FragmentGameList.kt
@@ -1,12 +1,16 @@
 package org.example.project
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -14,7 +18,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -26,25 +32,39 @@ import org.koin.androidx.compose.get
 @Composable
 fun FragmentGameList(navController: NavController) {
     val tablesRepository: AvailableGamesRepository = get()
-    //TODO fetch from api
-//    val items = listOf("Item 1", "Item 2", "Item 3") // This would be fetched from the API
     val items by remember {
         mutableStateOf(tablesRepository.getAvailableGames().map { result -> "${result.gameId} - ${result.status}" })
     }
 
-    LazyColumn {
-        items(items) { item ->
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(text = item)
-                CountdownTimer(remainingSeconds = 60) // Example countdown timer
-            }
+    Box(modifier = Modifier.fillMaxSize()) {
+        Row(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .fillMaxWidth()
+                .background(Color.LightGray)
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween // Space out elements to the edges
+        ) {
+            Text("Vreme izvlacenja", style = MaterialTheme.typography.h6)
+            Text("Preostalo za uplatu", style = MaterialTheme.typography.h6)
+        }
 
-            Divider()
+        LazyColumn(
+            Modifier.padding(top = 64.dp)
+        ) {
+            items(items) { item ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(text = item)
+                    CountdownTimer(remainingSeconds = 60) // Example countdown timer
+                }
+
+                Divider()
+            }
         }
     }
 }
