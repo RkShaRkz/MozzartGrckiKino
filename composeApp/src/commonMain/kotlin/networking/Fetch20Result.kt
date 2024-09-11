5fd29fcf205db6136e4a2ba3b7069437585ca785
RkShaRkz <darksidemetatron@gmail.com>
2024-09-11 18:48:20 +0200

    Add all necessary junk to Fetch20Result
    I wanted to skip deserializing this nonsense and just get them as strings,
    but noo, this dumbass framework can't grab things as literal strings or json objects
    and requires me to make full deserialization for this... so here it is.
    
    The object deserializes fine, the UI starts up fine, so i can get back to more UI work now.

--- composeApp/src/commonMain/kotlin/networking/Fetch20Result.kt
+++ composeApp/src/commonMain/kotlin/networking/Fetch20Result.kt
@@ -1,6 +1,8 @@
 package networking
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 
 /**
  * {
@@ -22,18 +24,105 @@ data class Fetch20Result(
     val gameId: Int,
     val drawId: Int,
     val drawTime: Long,
+    @SerialName("status")
     private val stringStatus: String,
     val drawBreak: Int,
     val visualDraw: Int,
-    val pricePointsJson: String,
-    val prizeCategoriesJsonArray: String,
-    val wagerStatisticsJson: String
+    @SerialName("pricePoints")
+    val pricePoints: PricePoints,
+    @SerialName("prizeCategories")
+    val prizeCategoriesList: List<PrizeCategory>? = null,
+    @SerialName("wagerStatistics")
+    val wagerStatisticsJson: WagerStatistics? = null
 ) {
+    @Transient
     val status: DrawStatus = when(stringStatus) {
         "active" -> DrawStatus.ACTIVE
         "future" -> DrawStatus.FUTURE
         else -> throw IllegalArgumentException("Status ${stringStatus} not covered by DrawStatus! Please add it")
     }
+
+    companion object {
+        // Needed to generate the serializer
+    }
 }
+
+/**
+ * "pricePoints": {
+ *             "addOn": [
+ *                 {
+ *                     "amount": 0.5,
+ *                     "gameType": "KenoCloseToWin"
+ *                 },
+ *                 {
+ *                     "amount": 0.5,
+ *                     "gameType": "KinoBonus"
+ *                 },
+ *                 {
+ *                     "amount": 0.5,
+ *                     "gameType": "SideBets"
+ *                 }
+ *             ],
+ *             "amount": 0.5
+ *         }
+ */
+@Serializable
+data class PricePoints(
+    @SerialName("addOn")
+    val addons: List<PricePointAddon>,
+    @SerialName("amount")
+    val amount: Float
+)
+
+@Serializable
+data class PricePointAddon(
+    @SerialName("amount")
+    val amount: Float,
+    @SerialName("gameType")
+    val gameType: String
+)
+
+/**
+ *             {
+ *                 "id": 1,
+ *                 "divident": 0,
+ *                 "winners": 0,
+ *                 "distributed": 0,
+ *                 "jackpot": 0,
+ *                 "fixed": 1,
+ *                 "categoryType": 2,
+ *                 "gameType": "KinoBonus"
+ *             }
+ */
+@Serializable
+data class PrizeCategory(
+    val id: Int,
+    val divident: Float,
+    val winners: Int,
+    val distributed: Float,
+    val jackpot: Float,
+    val fixed: Float,
+    val categoryType: Int,
+    val gameType: String
+)
+
+/**
+ * "wagerStatistics": {
+ *             "columns": 0,
+ *             "wagers": 0,
+ *             "addOn": []
+ *         }
+ */
+@Serializable
+data class WagerStatistics(
+    val columns: Int,
+    val wagers: Int,
+    val addOn: List<DummyValue?>? = null
+)
+
+@Serializable
+data class DummyValue(
+    val bla: Int    //hopefully these wont happen
+)
 
 enum class DrawStatus { ACTIVE, FUTURE }
