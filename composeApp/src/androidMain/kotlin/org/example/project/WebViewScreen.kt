d30201923ef46e617998c3f62763af65d7f344c6
RkShaRkz <darksidemetatron@gmail.com>
2024-09-12 15:43:04 +0200

    Fix webview fragment/screen
    Not only has this fixed it's full-screen width/height but also centering
    and it doesn't need a 'magic' top magin to fit below the toolbar now either
    
    It just works fine.

--- composeApp/src/androidMain/kotlin/org/example/project/WebViewScreen.kt
+++ composeApp/src/androidMain/kotlin/org/example/project/WebViewScreen.kt
@@ -1,5 +1,8 @@
 package org.example.project
 
+import android.util.AttributeSet
+import android.view.Gravity
+import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.compose.foundation.layout.Box
@@ -18,18 +21,28 @@ fun WebViewScreen(url: String) {
     val context = LocalContext.current
 
     Box(
-        modifier = Modifier
-            .fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
         AndroidView(
             factory = { ctx ->
                 WebView(ctx).apply {
+                    // Since this is a bridge between typical Android views and Compose, it allows
+                    // us to pull necessary android-ish garbage workarounds to fix the UI so...
+                    // Create LayoutParams and set them on the WebView, and center it properly
+                    val params = ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                    )
+                    layoutParams = params
+                    foregroundGravity = Gravity.CENTER
+
                     webViewClient = WebViewClient()
                     settings.javaScriptEnabled = true
                     loadUrl(url)
                 }
             },
+            // I dont think this modifier does anything or works good but lets leave it there
+            // I think the root cause was that 'foregroundGravity' defaults to START|TOP
             modifier = Modifier
                 .align(Alignment.Center)
                 .fillMaxSize() // Doesn't work good...
